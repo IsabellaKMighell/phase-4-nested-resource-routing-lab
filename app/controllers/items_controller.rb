347f777846c76f62aa160c59855428932e1c1640
Isabella Mighell <ikm4dm@virginia.edu>
2021-10-21 00:57:31 -0400

    commit tree 8c0ddcc2e24defa654c0648f72230d72cdacd91a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_resp
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def not_found_resp
+    render json: {error: "User's Items Not Found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
